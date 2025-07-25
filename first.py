c2db719475775f12808ed4229511010f7c53b1c9
Gururajsajjan01 <gururajsajjan03@gmail.com>
2025-07-25 14:26:50 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,18 @@
+
+numbers = []
+
+print("Enter 5 integers:")
+for i in range(5):
+    num = int(input(f"Enter integer {i+1}: "))
+    numbers.append(num)
+
+print("\nFirst element:", numbers[0])
+print("Last element:", numbers[-1])
+
+print("\nAll elements in the list:")
+for num in numbers:
+    print(num)
+
+numbers.reverse()
+print("\nReversed list:")
+print(numbers)
